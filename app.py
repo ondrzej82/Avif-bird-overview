7f36c4232b6e31af8c5e0ce18227414e8a3ecfc1
ondrzej82 <57037169+ondrzej82@users.noreply.github.com>
2025-02-23 17:35:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,6 @@ if selected_species not in ["Vyber", "Vše"]:
     fig_species_yearly.update_yaxes(dtick=max(1, yearly_species_counts["Počet pozorování"].max() // 5))
     if st.checkbox("Zobrazit graf počtu pozorování vybraného druhu v jednotlivých letech", value=True):
     st.plotly_chart(fig_species_yearly)
-    st.plotly_chart(fig_species_yearly)
 
 st.write("### 10 nejčastěji pozorovaných druhů")
 filtered_pie_data = df[(df["Datum"].dt.date >= date_from) & (df["Datum"].dt.date <= date_to)]
@@ -155,11 +154,9 @@ if not filtered_data.empty:
     fig1.update_yaxes(dtick=max(1, monthly_counts["Počet pozorování"].max() // 5))
     if st.checkbox("Zobrazit graf počtu pozorování podle měsíců", value=True):
     st.plotly_chart(fig1)
-    st.plotly_chart(fig1)
     fig2 = px.bar(monthly_counts, x="Měsíc", y="Počet jedinců", title="Počet jedinců podle měsíců", color_discrete_sequence=["red"])
     fig2.update_yaxes(dtick=max(1, monthly_counts["Počet jedinců"].max() // 5))
     if st.checkbox("Zobrazit graf počtu jedinců podle měsíců", value=True):
-    st.plotly_chart(fig2)
     st.plotly_chart(fig2)
 
 # Zobrazení filtrované tabulky
